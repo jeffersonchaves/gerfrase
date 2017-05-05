6ced8ab220bb19081e52bfd1a7fee1ae6a42150f
Jefferson Chaves <jeffersonchaves@users.noreply.github.com>
2017-05-05 09:28:02 -0300

    Upload dos arquivos iniciais da aplicação

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require 'logica.php'; ?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+    <h2> <?= "Olá $tratamento $nome, $qualidade $lugar" ?></h2>
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,10 @@
+<?php
+
+    $tratamentos = ['Dom', 'Samurai', 'Oráculo'];
+    $qualidades  = ['o Paladino', 'o colosso', 'o galinho de tênis'];
+    $lugares     = ['das terras médias', 'de Araquari', 'de Hogwarts'];
+    $nome        = "Jefferson";
+
+    $tratamento = $tratamentos[rand(0,2)];
+    $qualidade  = $qualidades[rand(0,2)];
+    $lugar      = $lugares[rand(0,2)];
